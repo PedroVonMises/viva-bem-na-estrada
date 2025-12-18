a08bdec9a32d24fdb6054527e94445045e8a7868
Pedro Augusto <pedrocodetheworld@gmail.com>
2025-12-18 19:31:58 -0300

    Vite Correction & AI on index

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
+import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  root: 'client', // Define a pasta client como raiz dos arquivos fontes
-  envDir: '..',   // Permite ler o arquivo .env da raiz do projeto
+  root: 'client',
+  envDir: '..',
   build: {
-    outDir: '../dist', // Gera a pasta dist na raiz do projeto, não dentro de client
-    emptyOutDir: true, // Limpa a pasta dist antes de gerar o novo build
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  resolve: {
+    alias: {
+      // Isso diz ao Vite que "@" aponta para a pasta "client/src"
+      "@": path.resolve(process.cwd(), "./client/src"),
+    },
   },
   plugins: [
     react(),
