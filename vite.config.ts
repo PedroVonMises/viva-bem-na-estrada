1e6b3053b3bba96d604df1520b02a4145b810e27
Pedro Augusto <pedrocodetheworld@gmail.com>
2025-12-18 19:16:55 -0300

    Vite config correction

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // <--- OBRIGATÓRIO: Importar isso
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  root: 'client', // Define a pasta client como raiz dos arquivos fontes
+  envDir: '..',   // Permite ler o arquivo .env da raiz do projeto
+  build: {
+    outDir: '../dist', // Gera a pasta dist na raiz do projeto, não dentro de client
+    emptyOutDir: true, // Limpa a pasta dist antes de gerar o novo build
+  },
   plugins: [
     react(),
-    tailwindcss(), // <--- OBRIGATÓRIO: Adicionar na lista de plugins
+    tailwindcss(),
   ],
 })
